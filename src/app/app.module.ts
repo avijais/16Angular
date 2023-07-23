a9a6f9798353e00c220853f44741714262161658
Avinash Jaiswal <Avinash.Jaiswal@users.noreply.github.com>
2023-07-24 01:25:37 +0530

    added incomplete org chart example2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighchartsChartModule } from 'highcharts-angular';
+import { OrganizationComponent } from './modules/chart-highchart/components/organization/organization.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizationComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/chart-highchart/chart-highchart.module.ts
+++ src/app/modules/chart-highchart/chart-highchart.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OrganizationComponent } from './components/organization/organization.component';
-
-
+// import { OrganizationComponent } from './components/organization/organization.component';
 
 @NgModule({
   declarations: [
-    OrganizationComponent
+    // OrganizationComponent
   ],
   imports: [
     CommonModule
