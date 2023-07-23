d2710b6fadc1c761ec05d4c4de205a83dbcfaeb8
Avinash Jaiswal <Avinash.Jaiswal@users.noreply.github.com>
2023-07-24 01:14:04 +0530

    created separate module for highcharts and added blank organization component

---
+++ src/app/modules/chart-highchart/chart-highchart.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrganizationComponent } from './components/organization/organization.component';
+
+
+
+@NgModule({
+  declarations: [
+    OrganizationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChartHighchartModule { }
