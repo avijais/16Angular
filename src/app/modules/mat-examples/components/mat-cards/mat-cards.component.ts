9815e5b1307d4b58513ec6455b9b3a9e0296984d
Avinash Jaiswal <Avinash.Jaiswal@users.noreply.github.com>
2023-07-25 23:49:22 +0530

    added blank mat-example module and mat-card component

---
+++ src/app/modules/mat-examples/components/mat-cards/mat-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-cards',
+  templateUrl: './mat-cards.component.html',
+  styleUrls: ['./mat-cards.component.scss']
+})
+export class MatCardsComponent {
+
+}
---
+++ src/app/modules/mat-examples/mat-examples.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardsComponent } from './components/mat-cards/mat-cards.component';
+
+
+
+@NgModule({
+  declarations: [
+    MatCardsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MatExamplesModule { }
